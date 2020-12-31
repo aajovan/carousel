d29810db5667c90bebc2bda078ebed14caa1e3f9
Alen <alenjovancom@gmail.com>
2020-12-31 17:55:40 +0100

    added popup

--- js/main.js
+++ js/main.js
@@ -67,3 +67,14 @@ $('input[name="autoplay"]').on('click', function (e) {
 
 });
 
+$('[popup-open]').on('click', function () {
+  var popup_name = $(this).attr('popup-open');
+  $('[popup-name="' + popup_name + '"]').fadeIn(300);
+});
+
+$('[popup-close]').on('click', function () {
+  var popup_name = $(this).attr('popup-close');
+  $('[popup-name="' + popup_name + '"]').fadeOut(300);
+});
+
+
