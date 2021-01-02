c976c1fc91ada1ba581cc39025f3c5370b218048
Alen <alenjovancom@gmail.com>
2021-01-02 18:09:16 +0100

    remove unnecessary function

--- js/main.js
+++ js/main.js
@@ -1,216 +1,215 @@
 "use strict";
 
-$(document).ready(function () {
-  var globalUI = {
-    popup: "#popup",
-    popupBtnOpen: ".popup__btn-open",
-    popupBtnClose: ".popup__btn-close",
-    toggleNav_mb: 'input[name="toggleNav-mb"]',
-    toggleNav_dt: 'input[name="toggleNav-dt"]',
-    autoplay_mb: 'input[name="autoplay-mb"]',
-    autoplay_dt: 'input[name="autoplay-dt"]',
-    autoplayNum_mb: 'input[name="autoplayNum-mb"]',
-    autoplayNum_dt: 'input[name="autoplayNum-dt"]',
-    carouselNav: ".carousel__nav",
-    carouselItem: ".carousel__item",
-    carouselNavImg: ".carousel__nav-img",
-    carouselNavActive: "carousel__nav-active",
-    carouselItemActive: "carousel__item-visible",
-    buttonSave: "#saveBtn",
-    prevBtn: "#carousel__button-prev",
-    nextBtn: "#carousel__button-next"
-  };
-  var mbConfig = {
-    autoplay: false,
-    hideNav: false,
-    time: 0
-  };
-  var dtConfig = {
-    autoplay: false,
-    hideNav: false,
-    time: 0
-  };
-  var totalSlides = $(globalUI.carouselItem).length;
-  var slidePos = 0;
-  var intervalAutoplay = null;
+var globalUI = {
+  popup: "#popup",
+  popupBtnOpen: ".popup__btn-open",
+  popupBtnClose: ".popup__btn-close",
+  toggleNav_mb: 'input[name="toggleNav-mb"]',
+  toggleNav_dt: 'input[name="toggleNav-dt"]',
+  autoplay_mb: 'input[name="autoplay-mb"]',
+  autoplay_dt: 'input[name="autoplay-dt"]',
+  autoplayNum_mb: 'input[name="autoplayNum-mb"]',
+  autoplayNum_dt: 'input[name="autoplayNum-dt"]',
+  carouselNav: ".carousel__nav",
+  carouselItem: ".carousel__item",
+  carouselNavImg: ".carousel__nav-img",
+  carouselNavActive: "carousel__nav-active",
+  carouselItemActive: "carousel__item-visible",
+  buttonSave: "#saveBtn",
+  prevBtn: "#carousel__button-prev",
+  nextBtn: "#carousel__button-next"
+};
+var mbConfig = {
+  autoplay: false,
+  hideNav: false,
+  time: 0
+};
+var dtConfig = {
+  autoplay: false,
+  hideNav: false,
+  time: 0
+};
+var totalSlides = $(globalUI.carouselItem).length;
+var slidePos = 0;
+var intervalAutoplay = null;
 
-  // init, apply default config
+// init, apply default config
 
-  slideNavigation();
-  checkIsMobile();
-  window.onresize = checkIsMobile;
+slideNavigation();
+checkIsMobile();
+window.onresize = checkIsMobile;
 
-  // setup next, prev btns
+// setup next, prev btns
 
-  $(globalUI.prevBtn).on("click", prevSlide);
-  $(globalUI.nextBtn).on("click", nextSlide);
+$(globalUI.prevBtn).on("click", prevSlide);
+$(globalUI.nextBtn).on("click", nextSlide);
 
-  // setup settings popup
+// setup settings popup
 
-  $(globalUI.popupBtnOpen).on("click", function () {
-    $(globalUI.popup).fadeIn(300);
-    loadSettings();
-  });
-  $(globalUI.popupBtnClose).on("click", function () {
-    $(globalUI.popup).fadeOut(300);
-  });
-
-  // setup save btn
-
-  $(globalUI.buttonSave).on("click", function () {
-    checkIsMobile();
-  });
-
-  // main function, setup carousel and carousel nav
-
-  function slideNavigation() {
-    $(globalUI.carouselItem).each(function (slide) {
-      var imgNavigation = document.createElement("img");
-      imgNavigation.classList.add("carousel__nav-img");
-      imgNavigation.setAttribute("src", $(this).find("img").attr("src"));
-
-      if (slide === slidePos) {
-        imgNavigation.classList.add(globalUI.carouselNavActive);
-      }
-
-      $(globalUI.carouselNav).append(imgNavigation);
-      imgNavigation.addEventListener("click", function () {
-        slidePos = slide;
-        displayActiveSlide();
-      });
-    });
-  }
-
-  // helper function - sets active carousel and nav item
-
-  function displayActiveSlide() {
-    displaySlide(globalUI.carouselItem, globalUI.carouselItemActive);
-    displaySlide(globalUI.carouselNavImg, globalUI.carouselNavActive);
-  }
-
-  // set active item
-
-  function displaySlide(element, className) {
-    $(element).each(function (item) {
-      item === slidePos
-        ? $(this).addClass(className)
-        : $(this).removeClass(className);
-    });
-  }
-
-  // go to next slide function
-
-  function nextSlide() {
-    if (slidePos === totalSlides - 1) {
-      slidePos = 0;
-    } else {
-      slidePos++;
-    }
-
-    displayActiveSlide();
-  }
-
-  // go to previous slide function
-
-  function prevSlide() {
-    if (slidePos === 0) {
-      slidePos = totalSlides - 1;
-    } else {
-      slidePos--;
-    }
-
-    displayActiveSlide();
-  }
-
-  // get and set navigation toggle data
-
-  function toggleNav(input, configItem, isCurrent) {
-    if ($(input).prop("checked") === true) {
-      if (isCurrent) {
-        $(globalUI.carouselNav).addClass("hidden");
-      }
-
-      configItem.hideNav = true;
-    } else {
-      if (isCurrent) {
-        $(globalUI.carouselNav).removeClass("hidden");
-      }
-
-      configItem.hideNav = false;
-    }
-  }
-
-  // get and set autoplay data
-
-  function toggleAutoplay(input, inputNum, configItem, isCurrent) {
-    var autoplayNum = Number($(inputNum).val());
-
-    if ($(input).prop("checked") === true && autoplayNum) {
-      if (isCurrent) {
-        clearInterval(intervalAutoplay);
-        intervalAutoplay = setInterval(nextSlide, autoplayNum);
-      }
-
-      configItem.autoplay = true;
-      configItem.time = autoplayNum;
-    } else {
-      if (isCurrent && intervalAutoplay) {
-        clearInterval(intervalAutoplay);
-        intervalAutoplay = null;
-      }
-
-      configItem.autoplay = false;
-      configItem.time = 0;
-      $(input).prop("checked", false);
-      $(inputNum).prop("value", 0);
-    }
-  }
-
-  // load popup settings
-
-  function loadSettings() {
-    $(globalUI.toggleNav_mb).prop("checked", mbConfig.hideNav);
-    $(globalUI.autoplay_mb).prop("checked", mbConfig.autoplay);
-    $(globalUI.autoplayNum_mb).prop("value", mbConfig.time);
-    $(globalUI.toggleNav_dt).prop("checked", dtConfig.hideNav);
-    $(globalUI.autoplay_dt).prop("checked", dtConfig.autoplay);
-    $(globalUI.autoplayNum_dt).prop("value", dtConfig.time);
-  }
-
-  // check if current window mobile or desktop
-  // apply user configuration
-
-  function checkIsMobile() {
-    if (window.innerWidth < 992) {
-      toggleNav($(globalUI.toggleNav_mb), mbConfig, true);
-      toggleNav($(globalUI.toggleNav_dt), dtConfig, false);
-      toggleAutoplay(
-        $(globalUI.autoplay_mb),
-        $(globalUI.autoplayNum_mb),
-        mbConfig,
-        true
-      );
-      toggleAutoplay(
-        $(globalUI.autoplay_dt),
-        $(globalUI.autoplayNum_dt),
-        dtConfig,
-        false
-      );
-    } else {
-      toggleNav($(globalUI.toggleNav_mb), mbConfig, false);
-      toggleNav($(globalUI.toggleNav_dt), dtConfig, true);
-      toggleAutoplay(
-        $(globalUI.autoplay_mb),
-        $(globalUI.autoplayNum_mb),
-        mbConfig,
-        false
-      );
-      toggleAutoplay(
-        $(globalUI.autoplay_dt),
-        $(globalUI.autoplayNum_dt),
-        dtConfig,
-        true
-      );
-    }
-  }
+$(globalUI.popupBtnOpen).on("click", function () {
+  $(globalUI.popup).fadeIn(300);
+  loadSettings();
 });
+$(globalUI.popupBtnClose).on("click", function () {
+  $(globalUI.popup).fadeOut(300);
+});
+
+// setup save btn
+
+$(globalUI.buttonSave).on("click", function () {
+  checkIsMobile();
+});
+
+// main function, setup carousel and carousel nav
+
+function slideNavigation() {
+  $(globalUI.carouselItem).each(function (slide) {
+    var imgNavigation = document.createElement("img");
+    imgNavigation.classList.add("carousel__nav-img");
+    imgNavigation.setAttribute("src", $(this).find("img").attr("src"));
+
+    if (slide === slidePos) {
+      imgNavigation.classList.add(globalUI.carouselNavActive);
+    }
+
+    $(globalUI.carouselNav).append(imgNavigation);
+    imgNavigation.addEventListener("click", function () {
+      slidePos = slide;
+      displayActiveSlide();
+    });
+  });
+}
+
+// helper function - sets active carousel and nav item
+
+function displayActiveSlide() {
+  displaySlide(globalUI.carouselItem, globalUI.carouselItemActive);
+  displaySlide(globalUI.carouselNavImg, globalUI.carouselNavActive);
+}
+
+// set active item
+
+function displaySlide(element, className) {
+  $(element).each(function (item) {
+    item === slidePos
+      ? $(this).addClass(className)
+      : $(this).removeClass(className);
+  });
+}
+
+// go to next slide function
+
+function nextSlide() {
+  if (slidePos === totalSlides - 1) {
+    slidePos = 0;
+  } else {
+    slidePos++;
+  }
+
+  displayActiveSlide();
+}
+
+// go to previous slide function
+
+function prevSlide() {
+  if (slidePos === 0) {
+    slidePos = totalSlides - 1;
+  } else {
+    slidePos--;
+  }
+
+  displayActiveSlide();
+}
+
+// get and set navigation toggle data
+
+function toggleNav(input, configItem, isCurrent) {
+  if ($(input).prop("checked") === true) {
+    if (isCurrent) {
+      $(globalUI.carouselNav).addClass("hidden");
+    }
+
+    configItem.hideNav = true;
+  } else {
+    if (isCurrent) {
+      $(globalUI.carouselNav).removeClass("hidden");
+    }
+
+    configItem.hideNav = false;
+  }
+}
+
+// get and set autoplay data
+
+function toggleAutoplay(input, inputNum, configItem, isCurrent) {
+  var autoplayNum = Number($(inputNum).val());
+
+  if ($(input).prop("checked") === true && autoplayNum) {
+    if (isCurrent) {
+      clearInterval(intervalAutoplay);
+      intervalAutoplay = setInterval(nextSlide, autoplayNum);
+    }
+
+    configItem.autoplay = true;
+    configItem.time = autoplayNum;
+  } else {
+    if (isCurrent && intervalAutoplay) {
+      clearInterval(intervalAutoplay);
+      intervalAutoplay = null;
+    }
+
+    configItem.autoplay = false;
+    configItem.time = 0;
+    $(input).prop("checked", false);
+    $(inputNum).prop("value", 0);
+  }
+}
+
+// load popup settings
+
+function loadSettings() {
+  $(globalUI.toggleNav_mb).prop("checked", mbConfig.hideNav);
+  $(globalUI.autoplay_mb).prop("checked", mbConfig.autoplay);
+  $(globalUI.autoplayNum_mb).prop("value", mbConfig.time);
+  $(globalUI.toggleNav_dt).prop("checked", dtConfig.hideNav);
+  $(globalUI.autoplay_dt).prop("checked", dtConfig.autoplay);
+  $(globalUI.autoplayNum_dt).prop("value", dtConfig.time);
+}
+
+// check if current window mobile or desktop
+// apply user configuration
+
+function checkIsMobile() {
+  if (window.innerWidth < 992) {
+    toggleNav($(globalUI.toggleNav_mb), mbConfig, true);
+    toggleNav($(globalUI.toggleNav_dt), dtConfig, false);
+    toggleAutoplay(
+      $(globalUI.autoplay_mb),
+      $(globalUI.autoplayNum_mb),
+      mbConfig,
+      true
+    );
+    toggleAutoplay(
+      $(globalUI.autoplay_dt),
+      $(globalUI.autoplayNum_dt),
+      dtConfig,
+      false
+    );
+  } else {
+    toggleNav($(globalUI.toggleNav_mb), mbConfig, false);
+    toggleNav($(globalUI.toggleNav_dt), dtConfig, true);
+    toggleAutoplay(
+      $(globalUI.autoplay_mb),
+      $(globalUI.autoplayNum_mb),
+      mbConfig,
+      false
+    );
+    toggleAutoplay(
+      $(globalUI.autoplay_dt),
+      $(globalUI.autoplayNum_dt),
+      dtConfig,
+      true
+    );
+  }
+}
+
